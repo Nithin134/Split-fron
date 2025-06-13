4198ddbd0f94e922a97737ac572511073faf5f98
Nithin134 <balajiraotirukkovalluri@example.com>
2025-06-13 15:02:00 +0530

    11

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://split-back-cz5q.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
