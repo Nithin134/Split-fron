232fce9e5d9e6d1c1d23c6cc5aa56575aad6cdb4
Nithin134 <133798305+Nithin134@users.noreply.github.com>
2025-07-16 14:50:21 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+   build: {
+    target: 'esnext',
+    minify: 'esbuild',
+    rollupOptions: {
+      treeshake: true,
+    }
+  plugins: [react(), visualizer()],
   server: { port: 3000 },
 });
